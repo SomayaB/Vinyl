4cd2f7e184050ba8fcbd7e9f4e2ef8cdc4d7990b
SomayaB <somaya.bounouar@gmail.com>
2017-10-16 12:13:35 -0700

    Separated routes into their own folder/file

---
+++ src/server/routes/albums.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const db = require('../../db');
+
+router.get('/:albumID', (req, res) => {
+  const albumID = req.params.albumID;
+
+  db.getAlbumsByID(albumID)
+  .then(albums => {
+    const album = albums[0];
+    res.render('album', {album});
+  })
+  .catch(error => {
+    res.status(500).render('error', {error});
+  });
+});
+
+module.exports = router;
---
+++ src/server/routes/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const db = require('../../db');
+const albums = require('./albums');
+
+
+router.get('/', (req, res) => {
+  db.getAlbums()
+  .then(albums => {
+    res.render('index', {albums});
+  })
+  .catch(error => {
+    res.status(500).render('error', {error});
+  });
+});
+
+
+router.use('/albums', albums);
+
+
+module.exports = router;
