87d2527e230307515fa976d6212bafb6145e07fd
SomayaB <somaya.bounouar@gmail.com>
2017-10-17 13:21:27 -0700

    Add seed data for users and reviews

---
+++ src/models/db/seed.sql
@@ -0,0 +1,16 @@
+INSERT INTO users
+  (name, email, encrypted_password)
+VALUES
+  ('Somaya', 'somaya@somaya.com', '$2a$10$dbOW6pfarskkvO5eW234OuHWW/hVAMUWmhHOokvfYvuWXvQefKLzS'),
+  ('Bernie', 'bernie@bernie.com', '$2a$10$R5U.DZ65POb9/kWqvR94DOnRIjU.p5iuZGU0I0TkuCvxwBrySQC0O')
+;
+
+
+INSERT INTO reviews
+  (content, user_id, album_id)
+VALUES
+  ('Malibu is an expansive opus that flows in multiple directions like a classic 70s double album.', 1, 1),
+  ('Solange’s new record is stunning, a thematically unified and musically adventurous statement on the pain and joy of black womanhood.', 2, 2),
+  ('Her second album is a masterful study of being a young woman, a sleek and humid pop record full of grief and hedonism, crafted with the utmost care and wisdom.', 1, 3),
+  ('In Rainbows represents the sound of Radiohead coming back to earth.', 2, 4)
+;
