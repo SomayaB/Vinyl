9872c15b62fe03a65965b5c03a2ddb16de7eab4e
SomayaB <somaya.bounouar@gmail.com>
2017-10-16 17:30:47 -0700

    Sign In functional

--- src/server/routes/authentication.js
+++ src/server/routes/authentication.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { createSession } = require('../utils');
+const { comparePasswords } = require('../../utils');
 const Users = require('../../models/users');
 
-router.get('/signup', (request, response) => {
+router.get('/sign-up', (request, response) => {
   if(request.session.user) {
     const id = request.session.user.id;
     response.redirect(`/users/${id}`); //TODO create route
@@ -11,7 +12,8 @@ router.get('/signup', (request, response) => {
   }
 });
 
-router.post('/signup', (request, response) => {
+
+router.post('/sign-up', (request, response) => {
   const name = request.body.name;
   const email = request.body.email;
   const password = request.body.password;
@@ -42,6 +44,44 @@ router.post('/signup', (request, response) => {
     });
   }
 });
+
+
+router.get('/sign-in', (request, response) => {
+  if(request.session.user) {
+    const id = request.session.user.id;
+    response.redirect(`/users/${id}`);
+  }
+    response.render('authentication/signin');
+  }
+);
+
+
+router.post('/sign-in', (request, response) => {
+  const email = request.body.email;
+  const password = request.body.password;
+  Users.findByEmail(email)
+  .then(user => {
+    comparePasswords(password, user.encrypted_password)
+    .then(passwordsMatch => {
+      if(passwordsMatch) {
+        createSession(request, response, user);
+        request.session.save(error => {
+          if(error) {
+            console.error(error);
+          } else {
+            response.redirect(`/users/${user.id}`);
+          }
+        });
+      } else {
+        response.render('authentication/signin', {warning: 'Incorrect email or password'});
+      }
+    });
+  })
+  .catch(error => {
+    response.render('authentication/signin', {warning: 'Incorrect email or password'});
+  });
+});
+
 
 router.get('/signout', (request, response) => {
   request.session.destroy((error) => {
