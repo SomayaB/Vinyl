22ac3c31369512ad20adb58f5c7d03186dceac12
SomayaB <somaya.bounouar@gmail.com>
2017-11-03 13:09:37 -0700

    Add initial image uploader and mail sender

--- public/script.js
+++ public/script.js
@@ -1,1 +1,6 @@
 console.log('hello from the browser JavaScript')
+
+var widget = uploadcare.Widget('[role=uploadcare-uploader]');
+widget.onUploadComplete(function(info) {
+  document.querySelector('.profile-img').src=info.cdnUrl
+});
